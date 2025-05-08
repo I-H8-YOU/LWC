549fb36b5bb3bed0337e5ed9e5e495647a7f7ba9
Jay <103248677+I-H8-YOU@users.noreply.github.com>
2025-05-08 17:53:47 +0900

    Create productItem.js

---
+++ step02-Component-Communication/productItem/productItem.js
@@ -0,0 +1,26 @@
+import { api, LightningElement } from 'lwc';
+
+export default class ProductItem extends LightningElement {
+    // @api productName;
+    __productName; //getter setter를 속성으로 접근하는것 만으로도 get set메소드 실행된다
+    //__변수명으로 따로 받아준다
+    @api productPrice;
+
+    @api set productName(value) {
+        this.__productName = value;
+        // this.productName = value; --> 이러면 set 무한 호출
+    }
+
+    get productName(){
+        return this.__productName;
+        // return this.productName;--> get 무한호출
+    }
+
+    handleClickAdd(){
+        const message = this.productName + " 추가 성공!";
+        let evt = new CustomEvent('add', {detail : message,
+            bubbles : true,
+        }); //customEvent 생성 .type, .detail 로 접근
+        this.dispatchEvent(evt); //부모 태그로 전달
+    }
+}
